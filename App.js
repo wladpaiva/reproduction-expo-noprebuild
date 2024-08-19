65cd985923614bee5ac00350eac8369cb1c9a839
Wlad Paiva <desenvolvedormg@gmail.com>
2024-08-19 14:14:51 -0300

    install custom module

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { ActivityAction, startActivityAsync } from "expo-intent-launcher";
+import { openApplication } from "expo-open-package";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text onPress={() => startActivityAsync(ActivityAction.WIFI_SETTINGS)}>
-        Open up App.js to start working on your app!
+        execute expo-intent-launcher
+      </Text>
+      <Text onPress={() => openApplication("com.facebook.katana")}>
+        execute expo-open-package
       </Text>
       <StatusBar style="auto" />
     </View>
